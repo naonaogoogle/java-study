d3c94fe7a193f60716cba617364706b7f286ed23
huanghaodong <1203891770hhd@gmail.com>
2018-12-08 19:13:14 +0800

    init

---
+++ demo/src/main/java/com/example/demo/util/event/InnerEventListener.java
@@ -0,0 +1,10 @@
+package com.example.demo.util.event;
+
+/**
+ * @Description:
+ * @Author: HuangHaodong
+ * @Date: 2018/12/8 19:12
+ */
+public class InnerEventListener {
+
+}
---
+++ demo/src/test/java/com/example/demo/DemoApplicationTest.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @Description:
+ * @Author: HuangHaodong
+ * @Date: 2018/11/17 00:33
+ */
+public class DemoApplicationTest {
+
+    @Test
+    public void getPerson() {
+
+    }
+}
